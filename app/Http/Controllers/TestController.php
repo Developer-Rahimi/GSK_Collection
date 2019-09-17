36f87be1fa2235c35a5c6ab05ba82d9d8727e631
moosa rahimi <moosarahimi8@gmail.com>
2019-09-17 09:23:32 +0430

    clean codes extra

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,8 +6,4 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    public function test()
-    {
-        Auth::loginUsingId(1);
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/test', 'HomeController@test');
+Route::get('/test', 'TestController@test');
 
 Auth::routes();
 
