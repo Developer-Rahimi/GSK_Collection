86d3dc4e1fe5bc54266f6361c4d038bebc0425ea
moosa rahimi <moosarahimi8@gmail.com>
2019-09-18 13:34:45 +0430

    define library fortawesome

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,7 +10,13 @@ window.Vue = require('vue');
 
 import Vue from 'vue'
 import EasySlider from 'vue-easy-slider'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(EasySlider)
 
 
