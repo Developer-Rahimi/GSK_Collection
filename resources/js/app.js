5b1da2e91a7501173f2287e723e538f93589961e
Developer-Rahimi <moosarahimi8@gmail.com>
2019-09-19 20:49:05 +0430

    ShowPage added

--- resources/js/app.js
+++ resources/js/app.js
@@ -27,6 +27,7 @@ Vue.component('Master', require('./components/Layout/Master.vue').default);
 
 Vue.component('About', require('./components/Page/About.vue').default);
 Vue.component('Contact', require('./components/Page/Contact.vue').default);
+Vue.component('ShowPage', require('./components/Page/ShowPage.vue').default);
 
 
 
