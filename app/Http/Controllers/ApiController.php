e8525c21455112f1694121af90eba318b8a8d8ce
Developer-Rahimi <moosarahimi8@gmail.com>
2019-09-19 22:06:29 +0430

    set utf8 in response

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,6 +9,11 @@ class ApiController extends Controller
 {
     public function Contact()
     {
-        return response()->json(['Address' => 'تهران-تهرانپارس-فلکه سوم خیابان 212 .... ', 'Email' => 'info@Gskcollection@Gmail.com', 'Phone' => '+989107608438', 'Fax' => '+98213333333']);
+        $data=['Address' => 'تهران-تهرانپارس-فلکه سوم خیابان 212 .... ',
+            'Email' => 'info@Gskcollection@Gmail.com',
+            'Phone' => '+989107608438',
+            'Fax' => '+98213333333'];
+        return response()->json($data, 200, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+            JSON_UNESCAPED_UNICODE);
     }
 }
