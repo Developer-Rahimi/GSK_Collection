eb597529a0f3940cefe5078a4cad910227bb2251
Developer-Rahimi <moosarahimi8@gmail.com>
2019-09-20 23:06:40 +0430

    update home page

--- resources/js/components/Page/Home.vue
+++ resources/js/components/Page/Home.vue
@@ -1,8 +1,35 @@
 <template>
-    <div>
-        <carousel :per-page="5">
+    <div  id="Product">
+        <span class="title">جدید ترین محصولات</span>
+        <carousel class="carousel"  :navigationEnabled="true" :per-page="5">
 
-                <slide ID="Product" v-for="Product in Products" v-bind:key="Product.ProductID">
+                <slide  v-for="Product in Products" v-bind:key="Product.ProductID">
+                    <div class="item">
+                        <img class="card-img" v-bind:src="'Images/'+Product.ProductIamge">
+                        <span class="title" v-text="Product.ProductName"></span>
+                        <span class="price" v-text="Product.ProductPrice+' تومان'"></span>
+                        <span class="btn_add_cart" >افزودن به سبد خرید</span>
+                    </div>
+                </slide>
+
+        </carousel>
+        <span class="title">پرفروش ترین محصولات</span>
+        <carousel class="carousel" navigationEnabled="true" :per-page="5">
+
+                <slide  v-for="Product in Products" v-bind:key="Product.ProductID">
+                    <div class="item">
+                        <img class="card-img" v-bind:src="'Images/'+Product.ProductIamge">
+                        <span class="title" v-text="Product.ProductName"></span>
+                        <span class="price" v-text="Product.ProductPrice+' تومان'"></span>
+                        <span class="btn_add_cart" >افزودن به سبد خرید</span>
+                    </div>
+                </slide>
+
+        </carousel>
+        <span class="title">تخفیفات ویژه</span>
+        <carousel class="carousel" navigationEnabled="true" :per-page="5">
+
+                <slide  v-for="Product in Products" v-bind:key="Product.ProductID">
                     <div class="item">
                         <img class="card-img" v-bind:src="'Images/'+Product.ProductIamge">
                         <span class="title" v-text="Product.ProductName"></span>
