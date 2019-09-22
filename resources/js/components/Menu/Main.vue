8d07b08aea3dc23e37744b014db5b844d353772c
Developer-Rahimi <moosarahimi8@gmail.com>
2019-09-22 11:30:39 +0330

    set icon for menu

--- resources/js/components/Menu/Main.vue
+++ resources/js/components/Menu/Main.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="menu">
-       <a v-for="menu in Menues" v-bind:href="menu.Link" v-bind:title="menu.Title"><span >{{menu.Title}}</span></a>
+       <a v-for="menu in Menues" v-bind:href="menu.Link" v-bind:title="menu.Title"><span >{{menu.Title}}</span><font-awesome-icon  v-bind:icon="menu.icon" /></a>
     </div>
 </template>
 
@@ -10,12 +10,12 @@
         data(){
             return {
                 Menues:[
-                    {Title:"صفحه اصلی",Link:"/"},
-                    {Title:"حساب من",Link:"#"},
-                    {Title:"مرکز دانلود",Link:"#"},
-                    {Title:"تماس با ما",Link:"/Contact"},
-                    {Title:"در باره ما",Link:"/About"},
-                    {Title:"خروج",Link:"#"},
+                    {Title:"صفحه اصلی",Link:"/",icon:"home"},
+                    {Title:"حساب من",Link:"#",icon:"user"},
+                    {Title:"مرکز دانلود",Link:"#",icon:"download"},
+                    {Title:"تماس با ما",Link:"/Contact",icon:"phone-square-alt"},
+                    {Title:"در باره ما",Link:"/About",icon:"info-circle"},
+                    {Title:"خروج",Link:"#",icon:"sign-out-alt"},
                 ],
             }
         },
