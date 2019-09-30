07ff87e254340388358025df8dcd6d1b20b26944
Developer-Rahimi <moosarahimi8@gmail.com>
2019-09-30 11:39:54 +0330

    Show data cart

--- resources/js/components/Page/Cart.vue
+++ resources/js/components/Page/Cart.vue
@@ -1,5 +1,22 @@
 <template>
-    <h1>Cart</h1>
+    <div class="container">
+        <div id="Cart">
+            <b-table :items="Carts" :fields="CartFields">
+                <template v-slot:cell(CartID)="data">
+                    {{ data.index + 1 }}
+                </template>
+                <template v-slot:cell(ProductName)="data">
+                    {{ data.item.Content.ContentName}}
+                </template>
+                <template v-slot:cell(ProductPrice)="data">
+                    {{ data.item.Content.Product.ProductPrice}}
+                </template>
+                <template v-slot:cell(Total)="data">
+                    {{ data.item.Content.Product.ProductPrice * data.item.Quntity }}
+                </template>
+            </b-table>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -14,6 +31,13 @@
         data(){
             return{
                 Carts:[],
+                CartFields:[
+                    { key: 'CartID', label: 'ردیف' },
+                    { key: 'ProductName', label: 'نام کالا' },
+                    { key: 'ProductPrice', label: 'قیمت(تومان)' },
+                    { key: 'Quntity', label: 'تعداد' },
+                    { key: 'Total', label: 'جمع(تومان)' },
+                ],
             }
         },
         mounted() {
