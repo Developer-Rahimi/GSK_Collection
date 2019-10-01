74c9b217c96d07b7cf306c8bfc4e3889b1b1bbaa
Developer-Rahimi <moosarahimi8@gmail.com>
2019-10-01 12:48:50 +0330

    set css for account page

--- resources/js/components/Page/Account.vue
+++ resources/js/components/Page/Account.vue
@@ -1,11 +1,12 @@
 <template>
-   <div id="container">
-       <div class="Account">
-           <h3>به حساب کاربری خود خوش آمدید. در اینجا میتوانید تمام اطلاعات شخصی و سفارشات خود را مدیریت نمائید.</h3>
+   <div class="container">
+       <div id="Account">
+           <h3 class="head">به حساب کاربری خود خوش آمدید. در اینجا میتوانید تمام اطلاعات شخصی و سفارشات خود را مدیریت نمائید.</h3>
         <ul>
             <li v-for="Item in Itemes">
-                <font-awesome-icon style="font-size: 70px;color:#4d90fe"  v-bind:icon="Item.icon" />
-                <span v-text="Item.Name"></span>
+                <a href=""></a>
+                <font-awesome-icon class="icon" style="font-size: 70px;color:#4d90fe"  v-bind:icon="Item.icon" />
+                <span class="name" v-text="Item.Name"></span>
             </li>
         </ul>
        </div>
@@ -21,7 +22,7 @@
                     {Name:"اطلاعات شخصی من",icon:"user",link:"#"},
                     {Name:"محصولات مورد علاقه",icon:"heart",link:"#"},
                     {Name:"تاریخچه و جزییات سفارش",icon:"history",link:"#"},
-                    {Name:"صورت های مالی من",icon:"money_bill_alt",link:"#"},
+                    {Name:"صورت های مالی من",icon:"money-bill-alt",link:"#"},
                     {Name:"آدرس های من",icon:"map-marker-alt",link:"#"},
                     {Name:"تخفیف های من",icon:"barcode",link:"#"},
                 ],
