84b7f8a1ee864c7142f4b3b23da0f6b67d0608fc
Developer-Rahimi <moosarahimi8@gmail.com>
2019-10-01 12:57:21 +0330

    update panel account

--- resources/js/components/Page/Account.vue
+++ resources/js/components/Page/Account.vue
@@ -1,14 +1,23 @@
 <template>
    <div class="container">
        <div id="Account">
-           <h3 class="head">به حساب کاربری خود خوش آمدید. در اینجا میتوانید تمام اطلاعات شخصی و سفارشات خود را مدیریت نمائید.</h3>
-        <ul>
-            <li v-for="Item in Itemes">
-                <a href=""></a>
-                <font-awesome-icon class="icon" style="font-size: 70px;color:#4d90fe"  v-bind:icon="Item.icon" />
-                <span class="name" v-text="Item.Name"></span>
-            </li>
-        </ul>
+           <div class="card">
+               <div class="card-header">
+                   <h2 class="title">حساب کاربری</h2>
+                   <h3 class="head">به حساب کاربری خود خوش آمدید. در اینجا میتوانید تمام اطلاعات شخصی و سفارشات خود را مدیریت نمائید.</h3>
+               </div>
+               <div class="card-body">
+                   <ul>
+                       <li v-for="Item in Itemes">
+                           <a href=""></a>
+                           <font-awesome-icon class="icon" style="font-size: 70px;color:#4d90fe"  v-bind:icon="Item.icon" />
+                           <span class="name" v-text="Item.Name"></span>
+                       </li>
+                   </ul>
+               </div>
+           </div>
+
+
        </div>
    </div>
 </template>
