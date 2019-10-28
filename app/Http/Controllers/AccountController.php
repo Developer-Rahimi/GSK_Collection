8f44bde007bf9ec823fb5381dc8b34a2b299886e
Developer <Moosarahimi8@gmail.com>
2019-10-28 07:47:55 +0330

    update login

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,17 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Session;
+
 
 class AccountController extends Controller
 {
     public function index()
     {
-        $Index=1;
+        $log=Session::get('Login');
+        $json=json_decode($log);
+        $Index=$json->id;
         return view("Pages.Account",compact('Index'));
     }
     public function Login()
     {
-        return view("Pages.Login");
+        $log=Session::exists('Login');
+        if($log){
+            return redirect('User/MyAccount');
+        }else {
+            return view("Pages.Login");
+        }
+
     }
     public function LogOut(Request $request)
     {
